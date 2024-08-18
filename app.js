758832bde50e2b94e9467ae2aefaef17cc00eee0
Mh-Monzil <monzil246@gmail.com>
2024-08-18 22:11:22 +0600

    update added

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const userModel = require('./usermodel')
+const userModel = require("./usermodel");
 
-app.get('/', async (req, res) => {
-    res.send("Welcome")
-})
+app.get("/", async (req, res) => {
+  res.send("Welcome");
+});
 
-app.get('/create', async (req, res) => {
-    let createUser = await userModel.create({
-        name: "monzil",
-        username: "mh-monzil",
-        email: "monzil@gmail.com"
-    })
-    res.send(createUser)
-})
+//create a new user
+app.get("/create", async (req, res) => {
+  let createUser = await userModel.create({
+    name: "monzil",
+    username: "mh-monzil",
+    email: "monzil@gmail.com",
+  });
+  res.send(createUser);
+});
 
-app.listen(3000, console.log("Running!"))
+app.get("/update", async (req, res) => {
+  let updatedUser = await userModel.findOneAndUpdate(
+    { username: "mh-monzil" },
+    { name: "ozil" },
+    { new: true }
+  );
+  res.send(updatedUser);
+});
+
+app.listen(3000, console.log("Running!"));
